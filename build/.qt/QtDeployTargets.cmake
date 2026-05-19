1bef2c2f063702f392243bd991fe880a43d960c3
cdstortz <cdstortz@gmail.com>
2026-05-18 21:57:26 -0600

    Fix tracked files — apply .gitignore properly

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_VirtualBench_FILE C:/Users/cdsto/Documents/VirtualBench/app/VirtualBench.exe)
-set(__QT_DEPLOY_TARGET_VirtualBench_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_VirtualBench_RUNTIME_DLLS C:/Qt/6.11.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Core.dll)
