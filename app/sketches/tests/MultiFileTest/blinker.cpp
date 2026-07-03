eab6a49fdb32943246d693b80b8a3fed852b5ed1
cdstortz <cdstortz@gmail.com>
2026-07-02 18:02:45 -0600

    deleted old tests

--- app/sketches/tests/MultiFileTest/blinker.cpp
+++
@@ -1,5 +0,0 @@
-#include "blinker.h"
-
-int next_interval(const BlinkPattern& p, bool is_on) {
-    return is_on ? p.on_ms : p.off_ms;
-}
--- app/sketches/tests/MultiFileTest/blinker.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-struct BlinkPattern {
-    int on_ms;
-    int off_ms;
-};
-
-int next_interval(const BlinkPattern& p, bool is_on);
